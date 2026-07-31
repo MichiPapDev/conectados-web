70b7e4fbf2e990c2568bb190eb4704c23340f18a
MichiPapDev <francovalverde51@gmail.com>
2026-07-31 01:05:10 -0500

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['conectados.dpdns.org'],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
